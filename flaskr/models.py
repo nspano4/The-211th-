bcddaae9dc83143fe035af90d8af2d7a032a8c76
jgraf <justin.graf8@gmail.com>
2020-04-06 10:25:52 -0400

    Started a database model file : models.py

---
+++ flaskr/models.py
@@ -0,0 +1,34 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+migrate = Migrate(app, dp)
+
+
+# Database model for the User entries
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20))
+    last_name = db.Column(db.String(30))
+    email = db.Column(db.String(50))
+    pass_hash = db.Column(String(100))
+    username = db.Column(String(25))
+
+
+# Database entries for the daily stock report entries
+class StockEntryDay(db.Model):
+    stock_id = db.Column(db.Integer, primary_key=True)
+    symbol = db.Column(db.String(60))
+    date = db.Column(db.String(60))
+    open_val = db.Column(db.Float)
+    close_val = db.Column(db.Float)
+    high_val = db.Column(db.Float)
+    lov_val = db.Column(db.Float)
+    volume = db.Column(db.int)
+
+
+# Database model for the stock entries
+class Stock(db.Model):
+    stock_id = db.Column(db.Integer, primary_key=True)
+    symbol = db.Column(db.String(60))
