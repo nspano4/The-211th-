4c4d53ce0c83c9d13e5246fe34c3aa1264811271
mcgroarym5 <mcgroarym5@students.rowan.edu>
2020-04-15 18:48:56 -0400

    updated to the new way we are running flask in order to run this off of AWS

--- flaskr/endpoints.py
+++ flaskr/endpoints.py
@@ -41,6 +41,10 @@ def products():
 def login():
     return render_template("login.html")
 
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
 if(__name__=="__main__"):
     #app.run(host=='0.0.0.0', port=5000)
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='127.0.0.1', port=5000)
