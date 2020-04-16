01927e6df5a6c011cff93cf3ddb778867301d66b
Justin Graf <47124919+jgraf3210@users.noreply.github.com>
2020-04-15 21:41:07 -0400

    Update endpoints.py
    Forgot to change the host to 127.0.0.1 when I made my commit

--- flaskr/endpoints.py
+++ flaskr/endpoints.py
@@ -74,4 +74,4 @@ def register():
 
 if(__name__=="__main__"):
     # app.run(host=='0.0.0.0', port=5000)
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='127.0.0.1', port=5000)
