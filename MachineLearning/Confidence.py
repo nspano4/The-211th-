ff56badc35ea488473b9551177004dfafb579c74
justinjarosz <33471372+justinjarosz@users.noreply.github.com>
2020-04-25 13:03:44 -0400

    Add files via upload
    Updated Confidence meter

--- MachineLearning/Confidence.py
+++ MachineLearning/Confidence.py
@@ -1,5 +1,6 @@
 import csv
 
+
 with open('testData.csv', 'r') as test:
     with open('results.csv', 'w') as results:
         with open('test1.csv', 'r') as test1:
@@ -7,24 +8,32 @@ with open('testData.csv', 'r') as test:
             writer = csv.writer(results)
             reader1 = csv.reader(test1)
 
-            writer.writerow(next(reader) + ['WARNING'])
+            writer.writerow(next(reader) + ['PERCENTAGE'])
             next(reader1)
+
+            total_percent = 0
+            num_loops = 0
 
             for row in reader:
                 predicted_val = row[1]
-                for row1 in reader1:
-                    exact = row1[1]
-                    highG = float(exact) * 1.05
-                    lowG = float(exact) * .95
+                if num_loops < 7:
+                    for row1 in reader1:
+                        exact = row1[1]
+                        percent_off = float(exact) / float(predicted_val)
 
-                    highY = float(exact) * 1.15
-                    lowY = float(exact) * .85
+                        total_percent = float(total_percent) + float(percent_off)
 
-                    if lowG <= float(predicted_val) <= highG:
-                        message = 'GREEN'
-                    elif lowY <= float(predicted_val) <= highY:
-                        message = 'YELLOW'
-                    else:
-                        message = 'RED'
-                    writer.writerow(row + [message])
-                    break
+                        writer.writerow(row + [float(percent_off)])
+                        break
+                    num_loops = num_loops + 1
+
+            final_percent = (total_percent / num_loops) * 100
+        with open('data.txt', 'w') as data:
+            if 95 <= final_percent <= 105:
+                data.write("We are very confident in the prediction.\n")
+            elif 85 <= final_percent <= 115:
+                data.write("We are somewhat confident in the prediction.\n")
+            else:
+                data.write("We are not very confident in the prediction.\n")
+
+            data.write("The stock's prediction have been {:f}% the actual value.".format(final_percent))
