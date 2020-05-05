9d3d32cc96374e82eceebdc4a84d967e820dbeae
jgraf <justin.graf8@gmail.com>
2020-05-04 20:52:16 -0400

    Added Ethan's files for merge

--- MachineLearning/main.py
+++ MachineLearning/main.py
@@ -1,21 +1,24 @@
 from MachineLearning.LinearRegression import *
 from call_database import *
+from MachineLearning.Scheduler import *
 
 
 
 # The main class will allow you to test some of the functionality of our project.
 def main():
-    tomorrowValues("AAPL")
-    #pullAllData()
-    #pullTodaysStockData("GOOGL")
-    #pullAllStockData("GOOGL")
-    #train()
-    #while True:
-    test("AAPL")
-    predictValue("Close", "MSFT")
+    pullAllData()
+    pullAllStockData("AAPL")
+    pullAllStockData("MSFT")
+    pullAllStockData("GOOGL")
+    train()
+    predictTomorrowValue("AAPL")
+    predictTomorrowValue("MSFT")
+    predictTomorrowValue("GOOGL")
 
 
 
 
 if __name__ == "__main__":
-    main()
+    while True:
+        while schedule():
+            main()
